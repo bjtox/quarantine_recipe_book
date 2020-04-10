a6008ea4860b25797c043ac66795be8f7e8a3ddf
Antonio Bitonti <antonio.bitonti@gmail.com>
2020-04-10 11:21:27 +0200

    fix hostname

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://d81qycyh67me9.cloudfront.net/'
+SITEURL = 'http://d2xyzgq61zh6ps.cloudfront.net/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
